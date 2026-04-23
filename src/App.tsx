21da7f230d99c8e8f8e4278d69c2385f28b17608
vedantgolegaonkar <vedantgolegaonkar@gmail.com>
2026-04-23 12:56:14 +0530

    Speed Insights added to vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 // import CustomCursor from './components/CustomCursor';
 
 function App() {
   return (
     <div className="bg-surface text-on-surface font-body selection:bg-primary selection:text-on-primary">
+      <Analytics />
+      <SpeedInsights />
       {/* <CustomCursor /> */}
       <Navbar />
       <div className="pt-20">
